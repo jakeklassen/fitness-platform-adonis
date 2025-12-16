4acad39c9510894e931b3c5e1bf50f9834aee2b4
John Klassen <klassenjo191@gmail.com>
2025-12-16 13:12:42 -0500

    Bring back better looking connect and disconnect buttons, adjust colors

--- packages/platform/.prettierrc.ts
+++ packages/platform/.prettierrc.ts
@@ -1,4 +1,5 @@
 // https://github.com/adonisjs/prettier-config
+// @ts-ignore - No types available for prettier config
 import adonisPrettierConfig from '@adonisjs/prettier-config';
 
 const config: import('prettier').Config = {
